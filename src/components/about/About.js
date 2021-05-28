7e7b2bdc01f4ab695216e41c1d7942cff9d529cd
Kevin Villanueva <kevin.322.vr@gmail.com>
2021-05-27 23:59:39 -0500

    Nosotros terminado, error en background-images corregido

--- src/components/about/About.js
+++ src/components/about/About.js
@@ -1,9 +1,27 @@
 import React from "react";
+import { Myv } from "./Myv";
 
 export const About = () => {
 	return (
-		<div>
-			<h2>About</h2>
-		</div>
+		<>
+			<div class="nosotros">
+				<div class="nosotros-title">
+					<h2>NOSOTROS</h2>
+				</div>
+				<div class="nosotros-logo">
+					<div class="nosotros-logo-container">
+						<img src="images/nosotros/LOGO_azul_HM23234.png" alt=""/>
+					</div>
+				</div>
+				<div class="nosotros-info">
+					<div class="nosotros-info-container">
+						Lorem ipsum dolor sit amet consectetur adipisicing elit. Vero quos sed facilis quisquam inventore ullam repellat animi at, numquam placeat, suscipit nobis! Numquam itaque consequatur incidunt repellat provident animi quos! Lorem, ipsum dolor sit amet
+						consectetur adipisicing elit. Perferendis doloribus quod assumenda beatae natus tempora eius ipsum! Libero sint unde repellat est molestiae mollitia tempore numquam officiis, ex dicta consequatur? Lorem ipsum dolor sit amet consectetur adipisicing
+						elit.
+					</div>
+				</div>
+			</div>
+			<Myv/>
+		</>
 	);
 };
---
+++ src/components/about/Myv.js
@@ -0,0 +1,30 @@
+import React from 'react'
+
+export const Myv = () => {
+    return (
+        <div class="myv">
+				<div class="mision">
+					<div class="mision-container">
+						<div class="mision-info">
+							<h3>MISION</h3>
+							<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Itaque, facilis saepe. Impedit, nobis. Laboriosam excepturi ea quasi provident dolores totam est facilis cumque, voluptates voluptatibus ratione fugiat, eaque sunt dolore!</p>
+						</div>
+						<div class="mision-img">
+							<img src="images/mision.jpg" alt=""/>
+						</div>
+					</div>
+				</div>
+				<div class="vision">
+					<div class="vision-container">
+						<div class="vision-img">
+							<img src="images/vision2.jpg" alt=""/>
+						</div>
+						<div class="vision-info">
+							<h3>VISION</h3>
+							<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Minima, incidunt id dolorum iure adipisci dolores magnam error ipsum labore minus est ea at cum quaerat fugit dolore officia provident voluptatum.</p>
+						</div>
+					</div>
+				</div>
+			</div>
+    )
+}
