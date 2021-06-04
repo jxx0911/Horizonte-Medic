879054bfad9d8dda69f1095a0b07491d7412c832
jxx0911 <kevin.322.vr@gmail.com>
2021-06-04 16:57:48 -0500

    Se subio a PRODUCCION, queda pendiente nuevos diseños para el home

--- src/components/about/Myv.js
+++ src/components/about/Myv.js
@@ -5,6 +5,9 @@ export const Myv = () => {
 		<div className="myv">
 			<div className="mision">
 				<div className="mision-container">
+					<div className="mision-img">
+						<img src="images/mision.jpg" alt="" />
+					</div>
 					<div className="mision-info">
 						<h3>MISION</h3>
 						<p>
@@ -14,16 +17,10 @@ export const Myv = () => {
 							voluptatibus ratione fugiat, eaque sunt dolore!
 						</p>
 					</div>
-					<div className="mision-img">
-						<img src="images/mision.jpg" alt="" />
-					</div>
 				</div>
 			</div>
 			<div className="vision">
 				<div className="vision-container">
-					<div className="vision-img">
-						<img src="images/vision2.jpg" alt="" />
-					</div>
 					<div className="vision-info">
 						<h3>VISION</h3>
 						<p>
@@ -32,6 +29,9 @@ export const Myv = () => {
 							labore minus est ea at cum quaerat fugit dolore officia provident
 							voluptatum.
 						</p>
+					</div>
+					<div className="vision-img">
+						<img src="images/vision2.jpg" alt="" />
 					</div>
 				</div>
 			</div>
